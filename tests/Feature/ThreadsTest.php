a83675746e5c561576e1eef6777bc442b0e9593e
Tong Gan <tonggan@myclubbetting.com>
2017-05-05 12:18:04 +0100

    implement test that user can see a thread title

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,11 +10,22 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
+    
     /** @test */
     public function a_user_can_browse_threads()
     {
         $response = $this->get('/threads');
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_user_can_see_the_thread_title_on_the_browser()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+
     }
 
 }
