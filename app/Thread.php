653625c1a257782a2aff8db9db673477cc32177b
Tong Gan <tonggan@myclubbetting.com>
2017-05-08 13:48:53 +0100

    Create channel which is an instance of Thread class

--- app/Thread.php
+++ app/Thread.php
@@ -13,14 +13,19 @@ class Thread extends Model
         return '/threads/' . $this->id;
     }
 
-    public function replies()
-    {
-        return $this->hasMany(Reply::class);
-    }
-
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 
     public function addReply($reply)
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -20,16 +20,16 @@ class ThreadTest extends TestCase
     }
 
     /** @test */
-    public function a_thread_has_replies()
-    {
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
-
-    }
-    
-    /** @test */
     public function a_thread_has_a_creator()
     {
         $this->assertInstanceOf('App\User', $this->thread->creator);
+
+    }
+
+    /** @test */
+    public function a_thread_has_replies()
+    {
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
 
     }
     
@@ -44,4 +44,13 @@ class ThreadTest extends TestCase
         $this->assertCount(1, $this->thread->replies);
     }
     
+    /** @test */
+    public function a_thread_belongs_to_a_channel()
+    {
+        $thread = create('App\Thread');
+
+        $this->assertInstanceOf('App\Channel', $thread->channel);
+
+
+    }
 }
