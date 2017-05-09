c4dfeef898509e04e65695ebcb0b927a525e7d51
Tong Gan <tonggan@myclubbetting.com>
2017-05-09 13:57:23 +0100

    Implement a new abstract class Filters

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -5,40 +5,11 @@ namespace App\Filters;
 use Illuminate\Http\Request;
 use App\User;
 
-class ThreadFilters
+class ThreadFilters extends Filters
 {
     /**
-     * @var Request
-     */
-    protected $request;
-    protected $builder;
-
-    /**
-     * ThreadFilters constructor.
-     * @param Request $request
-     */
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    /**
-     * @param $builder
-     * @return mixed
-     */
-    public function apply($builder)
-    {
-        $this->builder = $builder;
-
-        if ($this->request->has('by')) {
-            $this->by($this->request->by);
-        }
-
-        return $this->builder;
-    }
-
-    /**
-     * @param $username
+     * Filter the query by a given username.
+     * @param string $username
      * @return mixed
      */
     protected function by($username)
