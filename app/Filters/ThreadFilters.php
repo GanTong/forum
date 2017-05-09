10e1c1f603a9bd11479d381fdece531f39d7e465
Tong Gan <tonggan@myclubbetting.com>
2017-05-09 13:36:23 +0100

    Extract apply() to a new by() in ThreadsFilter

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -7,21 +7,41 @@ use App\User;
 
 class ThreadFilters
 {
+    /**
+     * @var Request
+     */
     protected $request;
 
+    /**
+     * ThreadFilters constructor.
+     * @param Request $request
+     */
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
-
+    /**
+     * @param $builder
+     * @return mixed
+     */
     public function apply($builder)
     {
         if (! $username = $this->request->by) return $builder;
 
-            $user = User::where('name', $username)->firstOrFail();
+        return $this->by($builder, $username);
 
-            return $builder->where('user_id', $user->id);
+    }
 
+    /**
+     * @param $builder
+     * @param $username
+     * @return mixed
+     */
+    protected function by($builder, $username)
+    {
+        $user = User::where('name', $username)->firstOrFail();
+
+        return $builder->where('user_id', $user->id);
     }
 }
