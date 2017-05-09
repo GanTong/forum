a0ce8b6d19fc3bf74ec0e86ebba9060b67f18f55
Tong Gan <tonggan@myclubbetting.com>
2017-05-09 10:32:33 +0100

    Tidy up and adding required validation helper in create blade

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,22 +13,27 @@
 
                             <div class="form-group">
                                 <label for="channel_id">Choose a Channel:</label>
-                                <select name="channel_id" id="channel_id" name="channel_id" class="form-control">
-                                    <option>Choose One...</option>
+                                <select name="channel_id" id="channel_id" name="channel_id" class="form-control" required>
+                                    <option value="">Choose One...</option>
                                         @foreach(App\Channel::all() as $channel)
-                                    <option value="{{ $channel->id }}">{{ $channel->name }}</option>
+                                            <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : ''}}>
+                                                {{ $channel->name }}
+                                            </option>
                                         @endforeach
                                 </select>
                             </div>
 
                             <div class="form-group">
                                 <label for="title">Thread Title:</label>
-                                    <input name="title" class="form-control" id="title" value="{{ old('title') }}">
+                                    <input name="title" class="form-control" id="title"
+                                           value="{{ old('title') }}" required>
                             </div>
 
                             <div class="form-group">
                                 <label for="body">Thread Body:</label>
-                                <textarea name="body" class="form-control" id="body" rows="8">{{ old('body') }}</textarea>
+                                    <textarea name="body" class="form-control" id="body"
+                                              rows="8" required>{{ old('body') }}
+                                    </textarea>
                             </div>
 
                             <div class="form-group">
